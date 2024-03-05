84269f6f19659f677792d3ba297aa9b9a6a3b748
mahamdeh0 <mahamdehabd@gmail.com>
2024-03-05 19:24:47 +0200

    done

--- DB/connection.js
+++ DB/connection.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
         console.log("Connected to DB");
     } catch (error) {
         console.error("Error connecting to DB:", error);
-    }
+    } 
 };
 
 module.exports = { connectDB };
