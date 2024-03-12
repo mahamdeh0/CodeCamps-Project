bdf09ddf0979d82183c510d7732ed535d1e51c10
Mohammad Zagha <tsu.test11@gmail.com>
2024-03-12 02:24:03 +0200

    validation middlewear caching fix

--- middlewear/validation.js
+++ middlewear/validation.js
@@ -1,33 +1,26 @@
-const methods = ['body','params','headers','query'];
+const methods = ['body', 'params', 'headers', 'query'];
 
-const validation = (shcema)=>{
-    let validarray = []
+const validation = (schema) => {
+    return (req, res, next) => {
+        let validarray = []; // Moved inside the function to ensure it's reset for each request
 
-    return (req,res,next)=>{
-
-        methods.forEach(key=>{
-
-            if(shcema[key]){
-                const valid = shcema[key].validate(req[key],{abortEarly:false});
-
-                if(valid?.error?.details){
-                    validarray.push(valid.error.details)
-                    
+        methods.forEach(key => {
+            if (schema[key]) {
+                const { error } = schema[key].validate(req[key], { abortEarly: false });
+                if (error) {
+                    error.details.forEach(detail => {
+                        validarray.push({ message: detail.message, path: detail.path });
+                    });
                 }
-
             }
-        })
- 
-        if(validarray.length > 0){
-            res.json({message:'validation error',err:validarray});
+        });
 
-        }else{
+        if (validarray.length > 0) {
+            res.status(400).json({ message: 'Validation error', errors: validarray });
+        } else {
             next();
         }
-
-    
     }
-} 
+};
 
-module.exports={validation}   
-
+module.exports = { validation };
