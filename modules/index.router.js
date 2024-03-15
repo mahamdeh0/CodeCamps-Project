0ff37ec7dd4f04be55ee253ebbefa1c46693819d
mahamdeh0 <mahamdehabd@gmail.com>
2024-03-15 10:44:24 +0200

    done

--- modules/index.router.js
+++ modules/index.router.js
@@ -2,4 +2,5 @@ const TeacherRouter = require('./Teacher/Teacher.router');
 const userRouter = require('./user/user.router');
 const adminRouter = require('./admin/admin.router');
 
-module.exports={TeacherRouter,userRouter,adminRouter};
+module.exports={TeacherRouter,userRouter,adminRouter}; 
+ 
